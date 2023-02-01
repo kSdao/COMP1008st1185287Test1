62b70590933f532a84411208dd015bd1c9f87b67
kSdao <kalemsdao03@gmail.com>
2023-02-01 09:02:37 -0500

    Question 3 Complete, Added set methods to the car class

--- src/main/java/com/example/comp1008st1185287test1/Car.java
+++ src/main/java/com/example/comp1008st1185287test1/Car.java
@@ -1,5 +1,8 @@
 package com.example.comp1008st1185287test1;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Car {
 
     private String make;
@@ -9,4 +12,41 @@ public class Car {
     private double price;
 
     private int year;
+
+
+    public void setMake(String make) {
+
+        List<String> validMakes = Arrays.asList("Ford","GMC","Honda","Nissan","BMW");
+
+        if (validMakes.contains(make))
+            this.make = make;
+        else
+            throw new IllegalArgumentException(make + "was not in the list of" + validMakes);
+
+        this.make = make;
+    }
+
+
+    public void setModel(String model) {
+        if (model.length() >= 2)
+            this.model = model;
+        else
+            throw new IllegalArgumentException("This is Not a Valid Model in our Database");
+    }
+
+
+    public void setPrice(double price) {
+        if (price >= 0 && price <= 150000)
+            this.price = price;
+        else
+            throw new IllegalArgumentException("Price must be between 0 and 150,000 dollars");
+    }
+
+
+    public void setYear(int year) {
+        if (year >= 1900 && year <= 2023);
+        this.year = year;
+        else
+         throw new IllegalArgumentException("Not a valid year in our system");
+    }
 }
