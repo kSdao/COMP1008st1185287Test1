e4ff908c02a53e5e5680a914a273ddfab9a7d3da
kSdao <kalemsdao03@gmail.com>
2023-02-01 08:42:59 -0500

    Question 2 Complete, Added instance Variables to each class

--- src/main/java/com/example/comp1008st1185287test1/Car.java
+++ src/main/java/com/example/comp1008st1185287test1/Car.java
@@ -1,4 +1,12 @@
 package com.example.comp1008st1185287test1;
 
 public class Car {
+
+    private String make;
+
+    private String model;
+
+    private double price;
+
+    private int year;
 }
--- src/main/java/com/example/comp1008st1185287test1/Dealership.java
+++ src/main/java/com/example/comp1008st1185287test1/Dealership.java
@@ -1,4 +1,12 @@
 package com.example.comp1008st1185287test1;
 
+import java.util.ArrayList;
+
 public class Dealership {
+
+private ArrayList<Car> inventory;
+
+
+
+
 }
